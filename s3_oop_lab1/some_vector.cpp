3ee7cd5bb728603cae3e7d083fa9012b56049763
serdenvl <serdenvl@gmail.com>
2022-09-08 15:03:05 +0700

    Добавьте файлы проекта.

---
+++ s3_oop_lab1/some_vector.cpp
@@ -0,0 +1,117 @@
+#include "some_vector.h"
+
+#include <iostream>
+using namespace std;
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <string>
+#include <sstream>
+
+namespace some_namespace {
+
+    double some_vector::get_x() const { return x; }
+    double some_vector::get_y() const { return y; }
+    double some_vector::get_z() const { return z; }
+
+    void some_vector::set_x(double val) { x = val; }
+    void some_vector::set_y(double val) { y = val; }
+    void some_vector::set_z(double val) { z = val; }
+
+    some_vector some_vector::operator=(const some_vector& other) const
+    {
+        return some_vector(other);
+    }
+
+    some_vector::operator std::string() const
+    {
+        return (stringstream() 
+            << "some_vector" 
+            << "[" << this << "]" 
+            << "(x = " << x << ", y = " << y << ", z = " << z << ")"
+            ).str();
+    }
+
+    double some_vector::len() const
+    {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+    some_vector some_vector::copy() const
+    {
+        return some_vector(x, y, z);
+    }
+
+    some_vector some_vector::scalar_prod(double a) const
+    {
+        return some_vector(x * a, y * a, z * a);
+    }
+
+    some_vector some_vector::norm() const
+    {
+        return scalar_prod(1 / len());
+    }
+
+    some_vector sum_vectors(const some_vector& a, const some_vector& b)
+    {
+        return some_vector(a.get_x() + b.get_x(), a.get_y() + b.get_y(), a.get_z() + b.get_z());
+    }
+
+    some_vector sub_vectors(const some_vector& a, const some_vector& b)
+    {
+        return some_vector(a.get_x() - b.get_x(), a.get_y() - b.get_y(), a.get_z() - b.get_z());
+    }
+
+    some_vector prod_vectors(const some_vector& a, const some_vector& b)
+    {
+        double comps_a[3] = { a.get_y(), a.get_z(), a.get_x() };
+        double comps_b[3] = { b.get_y(), b.get_z(), b.get_x() };
+
+        double buf[3];
+        for (int i = 0; i < 3; ++i)
+        {
+            buf[i] = comps_a[i] * comps_b[(i + 1) % 3] - comps_a[(i + 1) % 3] * comps_b[i];
+        }
+
+        return some_vector(*buf);
+    }
+
+    double scalar_prod_vectors(const some_vector& a, const some_vector& b)
+    {
+        double comps_a[3] = { a.get_x(), a.get_y(), a.get_z() };
+        double comps_b[3] = { b.get_x(), b.get_y(), b.get_z() };
+
+        double res = 0.0;
+        for (int i = 0; i < 3; ++i)
+            res += comps_a[i] * comps_b[i];
+
+        return res;
+    }
+
+    double cos_between_vectors(const some_vector& a, const some_vector& b)
+    {
+        double comps_a[3] = { a.get_x(), a.get_y(), a.get_z() };
+        double comps_b[3] = { b.get_x(), b.get_y(), b.get_z() };
+
+        double prod = 0.0;
+        for (int i = 0; i < 3; ++i)
+        {
+            prod += comps_a[i] * comps_b[i];
+        }
+
+        return prod / (a.len() * b.len());
+    }
+
+    double sin_between_vectors(const some_vector& a, const some_vector& b)
+    {
+        return sqrt(1 - pow(cos_between_vectors(a, b), 2));
+    }
+
+    double angle_between_vectors(const some_vector& a, const some_vector& b)
+    {
+        double get_x = cos_between_vectors(a, b);
+        double get_y = sin_between_vectors(a, b);
+        return atan2(get_y, get_x) * 180 / M_PI;
+    }
+}
---
+++ s3_oop_lab1/some_vector.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace some_namespace {
+
+    class some_vector
+    {
+    private:
+        double x, y, z;
+
+
+    public:
+        some_vector(double x = 0.0, double y = 0.0, double z = 0.0) : x(x), y(y), z(z)
+        {
+            std::cout << "Constructor " << std::string(*this) << std::endl;
+        }
+
+        some_vector(const some_vector& it) : x(it.get_x()), y(it.get_y()), z(it.get_z())
+        {
+            std::cout << "Constructor " << std::string(*this) << std::endl;
+        }
+
+        ~some_vector()
+        {
+            std::cout << "Destructor " << std::string(*this) << std::endl;
+        }
+
+        some_vector operator=(const some_vector& other) const;
+
+        double get_x() const;
+        double get_y() const;
+        double get_z() const;
+
+        void set_x(double val);
+        void set_y(double val);
+        void set_z(double val);
+
+        operator std::string() const;
+
+        double len() const;
+
+        some_vector copy() const;
+
+        some_vector scalar_prod(double a) const;
+
+        some_vector norm() const;
+    };
+
+    some_vector sum_vectors(const some_vector& a, const some_vector& b);
+
+    some_vector sub_vectors(const some_vector& a, const some_vector& b);
+
+    some_vector prod_vectors(const some_vector& a, const some_vector& b);
+
+    double scalar_prod_vectors(const some_vector& a, const some_vector& b);
+
+    double cos_between_vectors(const some_vector& a, const some_vector& b);
+
+    double sin_between_vectors(const some_vector& a, const some_vector& b);
+
+    double angle_between_vectors(const some_vector& a, const some_vector& b);
+
+}
